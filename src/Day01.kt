6fd2fcd72336037452c37c808e9c332bc3f988a9
shalva97 <shalva258@gmail.com>
2022-12-06 22:25:42 +0400

    day 1 complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val input = readInput("day1")
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    // part 1
+    input.fold(mutableListOf(mutableListOf())) { lists: MutableList<MutableList<Int>>, s: String ->
+        if (s.isNotEmpty()) {
+            lists.last().add(s.toInt())
+        } else {
+            lists.add(mutableListOf())
+        }
+        lists
+    }.maxOfOrNull { it.sum() }.println()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    // part 2
+    input.fold(mutableListOf(mutableListOf())) { lists: MutableList<MutableList<Int>>, s: String ->
+        if (s.isNotEmpty()) {
+            lists.last().add(s.toInt())
+        } else {
+            lists.add(mutableListOf())
+        }
+        lists
+    }.map { it.sum() }.sortedDescending().take(3).sum().println()
 }
