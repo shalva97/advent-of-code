f35617a591effd084bc974fccdd80d2b20a6b35e
shalva97 <shalva258@gmail.com>
2022-12-06 22:38:11 +0400

    clean

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,23 +2,16 @@ fun main() {
     val input = readInput("day1")
 
     // part 1
-    input.fold(mutableListOf(mutableListOf())) { lists: MutableList<MutableList<Int>>, s: String ->
-        if (s.isNotEmpty()) {
-            lists.last().add(s.toInt())
-        } else {
-            lists.add(mutableListOf())
-        }
-        lists
+    input.fold(mutableListOf(mutableListOf<Int>())) { acc, item ->
+        if (item.isNotEmpty()) acc.last().add(item.toInt())
+        else acc.add(mutableListOf())
+        acc
     }.maxOfOrNull { it.sum() }.println()
 
-
     // part 2
-    input.fold(mutableListOf(mutableListOf())) { lists: MutableList<MutableList<Int>>, s: String ->
-        if (s.isNotEmpty()) {
-            lists.last().add(s.toInt())
-        } else {
-            lists.add(mutableListOf())
-        }
-        lists
+    input.fold(mutableListOf(mutableListOf<Int>())) { acc, item ->
+        if (item.isNotEmpty()) acc.last().add(item.toInt())
+        else acc.add(mutableListOf())
+        acc
     }.map { it.sum() }.sortedDescending().take(3).sum().println()
 }
